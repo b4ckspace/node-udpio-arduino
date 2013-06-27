55693b557ef74094bc7546afdce28a0fb197013f
schinken <schinken@hackerspace-bamberg.de>
2013-06-27 17:39:12 +0200

    added udp broadcast prototype

---
+++ main.js
@@ -0,0 +1,28 @@
+var  settings = require('./settings.js')
+    ,firmata = require('./firmata-utils')
+    ,dram = require('dgram');
+
+var udp_client = dgram.createSocket('udp4');
+
+function write_udp(pin, value) {
+    var msg = new Buffer('<'+settings.prefix+':'+pin.pin+':'+value+'>');
+    udp_client.send(msg, 0, msg.length, settings.port, settings.ip);
+}
+
+var board = new firmata.Board(settings.serial, function() {
+
+    settings.watch.forEach(function(watch) {
+        
+        board.pinMode(watch.pin, firmata.MODES.INPUT);
+
+        if(watch.type == firmata.DIGITAL) {
+            var func = board.digitalDebounced;
+        } else if(watch.type == firmata.ANALOG) {
+            var func = board.analogRead;
+        }
+
+        func(watch.pin, function(value) {
+            write_udp(watch, value);
+        });
+    });
+});

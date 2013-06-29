362d1b688dae20307e640771441a6f28dccf3b5b
schinken <schinken@hackerspace-bamberg.de>
2013-06-29 18:04:28 +0200

    changed protocol, sending broadcast

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ var  settings = require('./settings.js')
     ,dgram = require('dgram');
 
 var udp_client = dgram.createSocket('udp4');
-var changelog = {};
 
-function write_udp(key, value, ip) {
-    ip = ip || settings.ip;
-    var msg = new Buffer('<'+settings.prefix+':'+key+':'+value+'>');
-    udp_client.send(msg, 0, msg.length, settings.port, ip);
+udp_client.bind(settings.port, '255.255.255.255', function() {
+    udp_client.setBroadcast(true);
+});
+
+var changelog = {};
+var reInitMessage = new RegExp(settings.prefix+',(\\d+),init,1');
+
+function sendBroadcastMessage(id, key, value) {
+    var msg = new Buffer([settings.prefix, id, key, value].join());
+    udp_client.send(msg, 0, msg.length, settings.port, '255.255.255.255');
 }
 
 var board = new firmata.Board(settings.serial, function() {
@@ -31,7 +36,7 @@ var board = new firmata.Board(settings.serial, function() {
 
         func(watch.pin, function(value) {
             if(!changelog[key] || changelog[key] != value) {
-                write_udp(key, value);
+                sendBroadcastMessage(0, key, value);
             }
             changelog[key] = value;
         });
@@ -40,9 +45,12 @@ var board = new firmata.Board(settings.serial, function() {
 
 // Send last state if init message is received to sending client
 udp_client.on('message', function(msg, info) {
-    if(msg == '<'+settings.prefix+':init>') {
-        this.changelog.forEach(function(val, key) {
-            write_udp(key, val, info.address);
+    var m = msg.toString().match(reInitMessage);
+    if(m){
+        var id = m[1];
+        Object.keys(changelog).forEach(function(key) {
+            var val = changelog[key];
+            sendBroadcastMessage(id, key, val);
         });
     }
 });

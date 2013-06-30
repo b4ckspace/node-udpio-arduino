0693f4c5067720d4ce4e28478fbe8558acc93c24
schinken <schinken@hackerspace-bamberg.de>
2013-06-30 18:27:41 +0200

    added winston logging

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 var  settings = require('./settings.js')
     ,firmata = require('./firmata-utils')
-    ,dgram = require('dgram');
+    ,dgram = require('dgram')
+    ,winston = require('winston');
+
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)({
+            timestamp: true,
+            colorize: true
+        })
+    ]
+});
+
 
 var udp_client = dgram.createSocket('udp4');
 
+logger.info('Binding to 255.255.255.255:'+settings.port);
 udp_client.bind(settings.port, '255.255.255.255', function() {
+    logger.info('Port bound, setting broadcast mode');
     udp_client.setBroadcast(true);
 });
 
@@ -13,6 +27,7 @@ var reInitMessage = new RegExp(settings.prefix+',(\\d+),init,1');
 
 function sendBroadcastMessage(id, key, value) {
     var msg = new Buffer([settings.prefix, id, key, value].join());
+    logger.info('sending pinchange '+key+' to '+value+' on channel '+id);
     udp_client.send(msg, 0, msg.length, settings.port, '255.255.255.255');
 }
 
@@ -48,6 +63,7 @@ udp_client.on('message', function(msg, info) {
     var m = msg.toString().match(reInitMessage);
     if(m){
         var id = m[1];
+        logger.info('received init from '+id);
         Object.keys(changelog).forEach(function(key) {
             var val = changelog[key];
             sendBroadcastMessage(id, key, val);
